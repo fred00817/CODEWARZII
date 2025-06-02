20c2da4485a3499f8aa3880e1901da48ad9b0aea
fred00817 <mucyofred00@gmail.com>
2025-06-02 10:40:55 +0200

    feat: finalised working properly

--- signs.js
+++ signs.js
@@ -1,11 +1,18 @@
 function neutralise(s1, s2) {
     // Here be dragons!
-   if(s1 && s2 === '+'){
-     return '+'
-     } else if (s1 && s2 === "-") { return '-'
-     } else if( s1 == "-" && s2 == "+"){
-       return 0
-     } 
+   let result = ''
+    for (let i = 0; i < s1.length; i++) {
+        let item1 = s1[i]
+        let item2 = s2[i]
+
+        if (item1 === item2) {
+            result += item1;
+        } else {
+            result += 0
+        }
+   
+    }
+    return result
 }
-      
-console.log(neutralise('-','+'))
+
+console.log(neutralise("--+", "+++"));
